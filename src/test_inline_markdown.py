e04e68990b57e9526136d940e84d3d14727fae22
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-09-20 10:03:44 -0500

    Added text to textnode function

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,6 +5,7 @@ from inline_markdown import (
     extract_markdown_images,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -167,6 +168,26 @@ class TextSplitImagesLink(unittest.TestCase):
             TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
             ],
             new_nodes,
+        )
+
+class testTexttoText(unittest.TestCase):
+    def test_text_to_textnode1(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            result       
         )
 
 if __name__ == "__main__":
