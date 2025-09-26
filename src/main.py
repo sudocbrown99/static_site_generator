9fcf594b60484bbe4c46ae22b6661e62eecae03e
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-09-26 14:57:33 -0500

    Finished generatign content function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 import os
 import shutil
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from markdown_blocks import markdown_to_html_node, markdown_to_blocks, block_to_block_type, BlockType
 
 def copy_static_dir(source_dir, dest_dir):
         source_list = os.listdir(source_dir)
@@ -17,6 +20,31 @@ def copy_static_dir(source_dir, dest_dir):
                     print(f"Creating Directory: {file_dest_path}")
                 copy_static_dir(full_path, file_dest_path)
 
+def extract_title(markdown):
+    mblocks = markdown_to_blocks(markdown)
+    for block in mblocks:
+        if block_to_block_type(block) == BlockType.HEADING and block.startswith("# "):
+            return block[2:].strip()
+    raise ValueError("No h1 found")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    with open(from_path, "r", encoding="utf-8") as f:
+        md = f.read()
+
+    with open(template_path, "r", encoding="utf-8") as f:
+        tpl = f.read()
+
+    html = markdown_to_html_node(md).to_html()
+    title = extract_title(md)
+    page = tpl.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    with open(dest_path, "w", encoding="utf-8") as f:
+        f.write(page)
+
 def main():
     if os.path.exists("./public"):
         shutil.rmtree("./public")
@@ -25,6 +53,7 @@ def main():
         os.mkdir("./public")
     
     copy_static_dir("./static", "./public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
     
 main()
